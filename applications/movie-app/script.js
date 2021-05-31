6a60a418061e11065ffe3d9a4495f83ecd411aa1
Yizhou Wang <yizhouw@nvidia.com>
2021-05-30 23:07:35 -0700

    movie app

---
+++ applications/movie-app/script.js
@@ -0,0 +1,54 @@
+const APIURI = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280/'
+
+const main = document.querySelector('main');
+
+async function getMovies() {
+    const resp = await fetch(APIURI);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    respData.results.forEach(movie => {
+        const { title, vote_average, poster_path, overview } = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}" 
+                alt = "${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">
+                ${vote_average}
+                </span>
+            </div>
+            <div class="overview">
+                <h4>Overview:</h4>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+
+
+    return respData;
+}
+
+getMovies();
+
+function getClassByRate(rate) {
+    if (rate >= 8) {
+        return 'green';
+    }
+    else if (rate >= 5) {
+        return 'orange';
+    }
+    else {
+        return 'red';
+    }
+}
